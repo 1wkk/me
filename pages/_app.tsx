b7a3a7d8526ad9c59fea60387db70abff78a4936
ryanmoyo <iherewithmyheart@gmail.com>
2022-02-21 20:21:55 +0800

    🚧 WIP index page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import '../styles/hljs.scss'
 import type { AppProps } from 'next/app'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <div className='antialiased'>
+    <Header />
+    <Component {...pageProps} />
+    <Footer />
+  </div>
+)
 
-export default MyApp
+export default App
