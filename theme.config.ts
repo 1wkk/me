1c158a5b19500feab8d2686e6d103a1693d7fd08
ryanmoyo <iherewithmyheart@gmail.com>
2022-02-21 20:21:43 +0800

    🔧 customize theme config

---
+++ theme.config.ts
@@ -0,0 +1,14 @@
+export const author = {
+  author: 'echo Eureka',
+  avatar: 'ryanmoyo',
+  twitter: 'cryanmoyo'
+}
+
+export const header = {
+  site: 'echo Eureka',
+  github: 'ryanmoyo'
+}
+
+export const footer = {
+  name: 'echo Eureka'
+}
