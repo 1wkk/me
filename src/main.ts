d83fa14580f636793450246e2537ae15f0b9713f
ryanmoyo <iherewithmyheart@gmail.com>
2022-03-31 14:55:30 +0800

    ⚡️ vite-ssg

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import { ViteSSG } from 'vite-ssg'
 
-createApp(App).mount('#app')
+export const createApp = ViteSSG(
+  // the root component
+  App,
+  // vue-router options
+  { routes },
+  // function to have custom setups
+  ({ app, router, routes, isClient, initialState }) => {
+    // install plugins etc.
+  }
+)
